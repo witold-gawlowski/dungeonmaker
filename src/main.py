a1f6ffb7942e7391f5df59fbbe737a316e08cc65
DESKTOP-BVN8U5J\Robert Doig <robertjdoig@hotmail.co.uk>
2016-12-16 03:35:13 +0000

    Added a stairs class and testing how to choose more then one mesh on a pass

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 import dungeon_generator
+import stairs_generator
+
 
 if __name__ == '__main__':
-  dg = dungeon_generator.dungeon_generator()
-  dg.read_components()
-  dg.write_result()
-
+  #dg = dungeon_generator.dungeon_generator()
+  #dg.read_components()
+  #dg.write_result()
+  
+  sg = stairs_generator.stairs_generator()
+  sg.read_components()
+  sg.write_result()
