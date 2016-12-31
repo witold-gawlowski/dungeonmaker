5b708ff213c54c95c3dceb991f44880b8d9b3aeb
DESKTOP-BVN8U5J\Robert Doig <robertjdoig@hotmail.co.uk>
2016-12-31 11:30:35 +0000

    Revert "Restructing the class layout, still need to connect the pass calls to the chamber generator class and then add specific stair and room classes"
    This reverts commit 32329c4d6a9021bfd0b5155406454862373ae3c8.

--- src/dungeon_generator.py
+++ src/dungeon_generator.py
@@ -12,10 +12,7 @@ import tile_filter
 import tile_util
 
 class dungeon_generator:
-  def __init__(self, tile_handler):  
-    self.tile_handler = tile_handler
-    
-    
+  def __init__(self):  
     self.sdk_manager = fbx.FbxManager.Create()
     if not self.sdk_manager:
       sys.exit(1)
@@ -339,7 +336,7 @@ class dungeon_generator:
     todo = self.create_todo(edges, nodes)
     print("Todo length: ", len(todo))
 
-    nodes = self.tile_handler.complete_todo(new_scene, todo, edges, nodes, None, "Floor_Wall_4x4x4", False)      
+    nodes = self.complete_todo(new_scene, todo, edges, nodes, None, "Floor_Wall_4x4x4", False)      
     print("Walls Complete!")
 
     print("Making Corners...")
