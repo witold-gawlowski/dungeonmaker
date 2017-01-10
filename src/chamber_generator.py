69f1584086548c2a52281aec821c287e249e1bc1
witold-gawlowski <witek.gawlowski@gmail.com>
2017-01-10 18:50:42 +0000

    specified the interface betwen dungeon and chamber generator in chamber_generator file. feel free to modyfiy.

--- src/chamber_generator.py
+++ src/chamber_generator.py
@@ -10,8 +10,11 @@ class chamber_generator:
     self.rg = room_generator.room_generator(tile_h) 
     self.sg = stairs_generator.stairs_generator(tile_h)
         
-  
-  def generate_chamber(self, scene):
+  # each chamber is in format of [ [pos_x, pos_y, pos_z] , [size_x, size_y, size_z] ]
+  # pos_xyz specify xyz coordinates of the corner of a chamber wih smallest coordinates.
+  # size is given in unites of tiles: 4x4x4
+  # door list is just a list of xyz coordinates of doors
+  def generate_chamber(self, scene, doors_list, chamber_list):
     cg = self.rg.create_room(scene, "Floor_Flat")
     # cg = self.sg.create_stairs(scene,"Floor_Flat")
     return cg
