66a041c41398a2c0a3259efc3385d8eb85602dd5
witold-gawlowski <witek.gawlowski@gmail.com>
2017-01-15 17:07:27 +0000

    finished implementing. run into some rte in room_gen.

--- src/dungeon_generator.py
+++ src/dungeon_generator.py
@@ -11,7 +11,7 @@ stair_widths = [5, 5, 7, 13]
 stair_heights = [3, 7, 11, 13, 17]
 level_bounds = [50, 50, 50]
 room_number = 5
-room_chance  = [0.5]
+room_chance  = 0.5
 room_attempts = 10
 
 class Chamber ( object ):
@@ -31,52 +31,58 @@ def random_chamber( is_room = True ):
   size = random_chamber_size( is_room )
   bounds = [ i - j for i, j in zip( level_bounds, size ) ]
   position = map ( lambda x: random.randint( *x ), [[0, bounds[0]], [0, bounds[1]], [0, bounds[2]]] )
-  return Chamber( position = tuple( position ), size = size, is_room = is_room )
+  return Chamber( position = tuple( position ), size = size, is_room = is_room, doors = [])
+
 
 def are_colliding( chamber_A , chamber_B ):
-  if chamber_A.position > chamber_B.position:
-    ( chamber_A, chamber_B ) = ( chamber_B, chamber_A )
-  for i in 0, 1, 2:
-    if chamber_B.position[ i ] > chamber_A.position[ i ] + chamber_A.size[ i ]:
-      return False
-  return True
+  a = chamber_A.position
+  A = map( sum , zip( a, chamber_A.size ) ) 
+  b = chamber_B.position
+  B = map( sum , zip( b, chamber_B.size ) ) 
+  overlap = lambda x, y : x[0] <= y[1] and x[1] >= y[0]
+  blend = zip( zip( a, A ), zip( b, B ) ) 
+  os = lambda x: overlap( *x )
+  return all( map( os, blend ) )
 
 
-
-def distance( chamber_A, chamber_B ):
+def tokyo_distance( chamber_A, chamber_B ):
   return sum( [ i - j for i, j in zip ( chamber_A.position, chamber_B.position ) ] )
 
 def get_chambers_from_world_file():
   return "todo"
 
-def doors_between( chamber_A, chaber_B ):
-  return "todo"
-
 def grow( chamber, direction ):
-  return "todo"
+  if direction < (0, 0, 0):
+    chamber.position = tuple( map( sum, zip( chamber.position, direction ) ) )
+  d =  zip( direction, direction ) 
+  direction2 = tuple( map( lambda x: x*x, direction ) )
+  s = chamber.size
+  tt = list( zip( chamber.size, direction2 ) )
+  chamber.size = tuple( map( sum, tt ) )
+  return chamber
 
 class dungeon_generator(object):
   def __init__( self, chamber_generator_instance ):
     self.chamber_generator_instance = chamber_generator_instance
     self.map = {}
     self.chambers = []
+    self.door_slots = {}
+    self.chamber_connections = []
 
   # room in format [[pos_x, pos_y, pos_z], [size_x, size_y, size_z]]
   # all dimensions in tile units
 
-  ''' inflates all the chambers, adds 
-  the encountered chambers to the pairs of rooms candidating for door addition '''
-
-  def generate_doors( self ):
-    for position in itertools.product( [ range( level_bound[ i ] - 1 ) for i in range( 2 ) ] ):
-      '''
-      add a dictionary of chambers
-      go over all level
-      and check if two neighboring tiles 
-      are not from different chambers but
-      chambers shifted by 1 smalle by 2
-      then add doors to those chambers
-      '''
+  def get_door_slots( self, chamber ):
+    size = chamber.size
+    pos = chamber.position
+    rangeX = ( pos[0] + 1, pos[0] + size[0] - 1 )
+    rangeY = ( pos[1] + 1, pos[1] + size[1] - 1 )
+    rangeZ = ( pos[2], pos[2] + size[2] - 1 )
+    wallX1 = [ ( i, pos[1], j ) for i in range( * rangeX ) for j in range( *rangeZ )  ]
+    wallX2 =[ ( i, pos[1] + size[1] - 1, j ) for i in range( * rangeX ) for j in range( *rangeZ ) ]
+    wallY1 =[ ( pos[0], i, j ) for i in range( * rangeY ) for j in range( *rangeZ )  ]
+    wallY2 = [ ( pos[0] + size[0] - 1, i, j ) for i in range( * rangeY ) for j in range( *rangeZ ) ]
+    return wallX1 +  wallX2 + wallY1 + wallY2    
 
   def grow_chambers( self ):
     grown_last_iteration = True
@@ -85,30 +91,55 @@ class dungeon_generator(object):
       for chamber in self.chambers:
         for direction in -1, 1:
           for axis in 0, 1, 2:
-            dir = (0, 0, 0)
+            ''' don't grow up! '''
+            if axis == 2 and direction == 1:
+              break
+            dir = [0, 0, 0]
             dir[axis] = direction
-            grown = grow( chamber, dir )
-            if not collides( grown ):
+            grown = grow( chamber, tuple( dir ) )
+            if not self.collides( grown ):
               chamber = grown
               grown_last_iteration = True
       
 
-  def collides( chamber ):
+  def collides( self, chamber ):
     for other in self.chambers:
       if are_colliding( chamber, other ):
         return True
     return False
 
+  def pull_down( self, pos, new_pos ):
+    ''' move the door as low as possible 
+    but within the rooms they are currently in '''
+    return ( pos, new_pos )
+
+  def add_doors( self ):
+    for chamber in self.chambers:
+      slots = self.get_door_slots ( chamber )
+      pairs = itertools.product( slots, [chamber] )
+      self.door_slots.update( pairs )
+     
+    for pos, chamber in self.door_slots.items():
+      for d in [(-1, 0, 0), (1, 0, 0), (0, 1, 0), (0, -1, 0)]:
+        new_pos = map( sum, zip( d, pos ) )
+        neighbour = self.door_slots.get( new_pos )
+        if neighbour and neighbour is not chamber:
+          if ( neighbour, chamber ) not in self.chamber_connections:
+            ( pos, new_pos ) = self.pull_down( pos, new_pos )
+            chamber.doors.append( pos )
+            neighbour.doors.append( new_pos )
+            self.chamber_connections.append( ( chamber, neighbour ) )
+            self.chamber_connections.append( ( neighbour, chamber ) )
+
   def generate_world_data( self ):
     for i in range( room_attempts ):
       candidate = random_chamber( True if random.random() < room_chance else False )
-      if ( collides ( candidate ) ):
+      if ( self.collides ( candidate ) ):
         continue
       self.chambers.append( candidate ) 
-      
       self.grow_chambers()
-
       self.add_doors()
+
   
   def build_from_world_file( self ):
     '''
@@ -128,9 +159,7 @@ class dungeon_generator(object):
     # self.build_from_world_file()
     self.generate_world_data ()
     nodes = self.build()
-    return nodes
-
-    
+    return nodes 
 
   def copy_node_with_children( self, source , node_processor = lambda x: x):
     dest_node = fbx.FbxNode.Create( self.scene, source.GetName() )
