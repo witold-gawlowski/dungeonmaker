73daf6ca0115c43d21f9fec7f2bc74700478d99d
witold-gawlowski <witek.gawlowski@gmail.com>
2017-01-17 15:48:38 +0000

    doors pulled.

--- src/dungeon_generator.py
+++ src/dungeon_generator.py
@@ -1,15 +1,17 @@
 import fbx_io
 import chamber_generator
 import utils
-import fbx
+# import fbx
 import random
 import itertools
+import copy
+
 
 room_widths = [3, 3, 3]# , 5, 5, 5, 7, 7, 11]
 room_heights = [2, 3, 3]#, 4, 4]
 stair_widths = [5, 5]#, 7, 13]
 stair_heights = [3, 5]#, 11, 13, 17]
-level_bounds = [30, 30, 30]
+level_bounds = [10, 7, 7]
 room_number = 5
 room_chance  = 0.5
 room_attempts = 10
@@ -33,16 +35,23 @@ def random_chamber( is_room = True ):
   position = map ( lambda x: random.randint( *x ), [[0, bounds[0]], [0, bounds[1]], [0, bounds[2]]] )
   return Chamber( position = tuple( position ), size = size, is_room = is_room, doors = [])
 
+def overlap2( t ):
+  x = t[0]
+  y = t[1]
+  result = x[0] <= y[1] and x[1] >= y[0]
+  return result
 
 def are_colliding( chamber_A , chamber_B ):
   a = chamber_A.position
-  A = map( sum , zip( a, chamber_A.size ) ) 
+  A = [a[i] + chamber_A.size[i] - 1 for i in range(3)]
   b = chamber_B.position
-  B = map( sum , zip( b, chamber_B.size ) ) 
-  overlap = lambda x, y : x[0] <= y[1] and x[1] >= y[0]
-  blend = zip( zip( a, A ), zip( b, B ) ) 
-  os = lambda x: overlap( *x )
-  return all( map( os, blend ) )
+  B = [b[i] + chamber_B.size[i] - 1 for i in range(3)] 
+  # overlap = lambda x, y : x[0] <= y[1] and x[1] >= y[0]
+  blend = zip( tuple (zip( a, A )), tuple(zip( b, B )) )
+  # os = lambda x: overlap( *x )
+  result = all( map( overlap2, blend ) )
+  return result
+
 
 
 def tokyo_distance( chamber_A, chamber_B ):
@@ -51,15 +60,8 @@ def tokyo_distance( chamber_A, chamber_B ):
 def get_chambers_from_world_file():
   return "todo"
 
-def grow( chamber, direction ):
-  if direction < (0, 0, 0):
-    chamber.position = tuple( map( sum, zip( chamber.position, direction ) ) )
-  d =  zip( direction, direction ) 
-  direction2 = tuple( map( lambda x: x*x, direction ) )
-  s = chamber.size
-  tt = list( zip( chamber.size, direction2 ) )
-  chamber.size = tuple( map( sum, tt ) )
-  return chamber
+
+  
 
 class dungeon_generator(object):
   def __init__( self, chamber_generator_instance ):
@@ -84,6 +86,22 @@ class dungeon_generator(object):
     wallY2 = [ ( pos[0] + size[0] - 1, i, j ) for i in range( * rangeY ) for j in range( *rangeZ ) ]
     return wallX1 +  wallX2 + wallY1 + wallY2    
 
+  def grow( self, chamber, direction ):
+    old_pos = chamber.position
+    old_size = chamber.size
+    if direction < (0, 0, 0):
+      chamber.position = tuple( map( sum, zip( chamber.position, direction ) ) )
+    direction2 = tuple( map( lambda x: x*x, direction ) )
+    s = chamber.size
+    tt = list( zip( chamber.size, direction2 ) )
+    chamber.size = tuple( map( sum, tt ) )
+    if self.collides( chamber ):
+      chamber.size = old_size
+      chamber.position = old_pos
+      return False
+    else:
+      return True
+
   def grow_chambers( self ):
     grown_last_iteration = True
     while grown_last_iteration:
@@ -96,19 +114,39 @@ class dungeon_generator(object):
               break
             dir = [0, 0, 0]
             dir[axis] = direction
-            grown = grow( chamber, tuple( dir ) )
-            if not self.collides( grown ):
-              chamber = grown
+            if self.grow( chamber, tuple( dir ) ):
               grown_last_iteration = True
       
-
+  
   def collides( self, chamber ):
+    within = lambda x: x[0][0] <= x[1][0] and  x[0][1] >= x[1][1]
+    ranges = [ ( ( 0, level_bounds[ i ] ), ( chamber.position[i], chamber.position[i] + chamber.size[i] - 1) ) for i in range(3)]
+    ll = list ( map( within, ranges ) )
+    # val = not all(  ll )
+    for v in ll:
+      if not v:
+        return True
     for other in self.chambers:
+      if other == chamber:
+        continue
       if are_colliding( chamber, other ):
         return True
     return False
 
-  def pull_down( self, pos, new_pos ):
+  def pull_down( self, pos, new_pos, chamb, neigh ):
+    pos_list = list (pos )
+    new_pos_list = list (new_pos)
+    while True:
+      pos_list[2] -= 1;
+      new_pos_list[2] -= 1;
+      if self.door_slots.get(tuple(pos_list)) != chamb or self.door_slots.get(tuple(new_pos_list)) != neigh:
+        break;
+    pos_list[2] += 1;
+    new_pos_list[2] += 1;
+    return (tuple(pos_list), tuple(new_pos_list))
+
+
+
     ''' move the door as low as possible 
     but within the rooms they are currently in '''
     return ( pos, new_pos )
@@ -126,74 +164,34 @@ class dungeon_generator(object):
         if neighbour:
           if neighbour is not chamber:
             if ( neighbour, chamber ) not in self.chamber_connections:
-              ( pos, new_pos ) = self.pull_down( pos, new_pos )
-              chamber.doors.append( pos )
-              neighbour.doors.append( new_pos )
+              ( pos, new_pos ) = self.pull_down( pos, new_pos, chamber, neighbour)
+              pos_abs = tuple ( [pos[i] - chamber.position[i] for i in range(3)] )
+              new_pos_abs = tuple ( [new_pos[i] - neighbour.position[i] for i in range(3)] )
+              chamber.doors.append( pos_abs )
+              neighbour.doors.append( new_pos_abs )
               self.chamber_connections.append( ( chamber, neighbour ) )
               self.chamber_connections.append( ( neighbour, chamber ) )
 
-
   def generate_world_data( self ):
-    chamber1 = Chamber()
-    chamber2 = Chamber()
-    chamber3 = Chamber()
-    chamber4 = Chamber()
-    chamber5 = Chamber()
-    chamber6 = Chamber()
-
-    chamber1.position = (10, 10, 0)
-    chamber1.size = (5, 4, 3)
-    chamber1.doors = []
-    chamber1.doors.append( ( 3, 3, 0) )
-    chamber1.doors.append( ( 4, 1, 0) )
-
-    chamber2.position = (12, 14, 0)
-    chamber2.size = (3, 3, 4)
-    chamber2.doors = []
-    chamber2.doors.append( ( 1, 0, 0 ) )
-    chamber2.doors.append( ( 2, 1, 0 ) )
-
-    chamber3.position = (15, 10, 0)
-    chamber3.size = (4, 7, 6)
-    chamber3.doors = []
-    chamber3.doors.append( ( 0, 1, 0 ) )
-    chamber3.doors.append( ( 0, 5, 0 ) )
-    chamber3.doors.append( ( 2, 6, 0 ) )
-
-    chamber4.position = (10, 17, 0)
-    chamber4.size = (9, 4, 8)
-    chamber4.doors = []
-    chamber4.doors.append( ( 1, 3, 0 ) )
-    chamber4.doors.append( ( 7, 0, 0 ) )
-    chamber4.doors.append( ( 8, 2, 0 ) )
-
-    chamber5.position = (10, 21, 0)
-    chamber5.size = (3, 4, 4)
-    chamber5.doors = []
-    chamber5.doors.append( ( 1, 0, 0 ) )
-
-    chamber6.position = (19, 18, 0)
-    chamber6.size = (4, 7, 6)
-    chamber6.doors = []
-    chamber6.doors.append( ( 0, 1, 0 ) )
-
-    self.chambers = []
-    self.chambers.append(chamber1)
-    self.chambers.append(chamber2)
-    self.chambers.append(chamber3)
-    self.chambers.append(chamber4)
-    self.chambers.append(chamber5)
-    self.chambers.append(chamber6)
-    print (self.chambers)
-
-  def generate_world_data2( self ):
     for i in range( room_attempts ):
       candidate = random_chamber( True if random.random() < room_chance else False )
       if ( self.collides ( candidate ) ):
         continue
-      self.chambers.append( candidate ) 
+      self.chambers.append( candidate )
+
     self.grow_chambers()
-    self.add_doors()
+
+
+    self.add_doors()   
+    for chamb in self.chambers:
+      print ("chamber: ")
+      print( chamb.position )
+      print( chamb.size )
+      for d in chamb.doors:
+        print( d )
+      print( " " )
+
+   
       
     ''' remove chambers with no doors'''
     self.chambers = [ x for x in self.chambers if len( x.doors ) > 0 ]
